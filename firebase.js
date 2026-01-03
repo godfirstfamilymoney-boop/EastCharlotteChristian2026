feb2da49969ae59540dd271c9a3357b0368260b9
godfirstfamilymoney-boop <godfirstfamilymoney@gmail.com>
2026-01-02 23:04:54 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,35 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-import {
-  getFirestore,
-  doc, getDoc, setDoc, updateDoc,
-  collection, addDoc, getDocs, query, where, orderBy,
-  serverTimestamp
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-
-export const firebaseConfig = {
-  apiKey: "REPLACE",
-  authDomain: "REPLACE",
-  projectId: "REPLACE",
-  storageBucket: "REPLACE",
-  messagingSenderId: "REPLACE",
-  appId: "REPLACE"
+const firebaseConfig = {
+  apiKey: "AIzaSyDZZRr754hFPKalq3MBdEv0VDheG_vPLe0",
+  authDomain: "east-charlotte-christian-2026.firebaseapp.com",
+  projectId: "east-charlotte-christian-2026",
+  storageBucket: "east-charlotte-christian-2026.firebasestorage.app",
+  messagingSenderId: "358859491654",
+  appId: "1:358859491654:web:f14516a4eb2b3156cabd47"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-export {
-  onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut,
-  doc, getDoc, setDoc, updateDoc,
-  collection, addDoc, getDocs, query, where, orderBy, serverTimestamp
-};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
