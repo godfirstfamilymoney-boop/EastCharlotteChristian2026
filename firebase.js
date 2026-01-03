b1c8a4e27706ca97121567d47f1243c6a864de7c
godfirstfamilymoney-boop <godfirstfamilymoney@gmail.com>
2026-01-02 22:27:12 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZZRr754hFPKalq3MBdEv0VDheG_vPLe0",
+  authDomain: "east-charlotte-christian-2026.firebaseapp.com",
+  projectId: "east-charlotte-christian-2026",
+  storageBucket: "east-charlotte-christian-2026.firebasestorage.app",
+  messagingSenderId: "358859491654",
+  appId: "1:358859491654:web:644a846cba393507cabd47"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
